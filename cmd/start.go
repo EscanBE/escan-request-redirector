c1facf873b4c0d069d4e47e490a9ba64a91bca6b
VictorTrustyDev <me@victortrusty.dev>
2024-04-23 08:08:23 +0700

    automatically detect engine

--- cmd/start.go
+++ cmd/start.go
@@ -5,6 +5,7 @@ import (
 	"github.com/EscanBE/escan-request-redirector/constants"
 	"github.com/spf13/cobra"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 )
@@ -23,10 +24,22 @@ var startCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		targetBlockExplorer := strings.TrimSuffix(args[0], "/")
-		redirectMessage, _ := cmd.Flags().GetString(flagRedirectMessage)
 		port, _ := cmd.Flags().GetUint16(flagPort)
 		redirectTimeout, _ := cmd.Flags().GetDuration(flagRedirectTimeout)
+
 		engine, _ := cmd.Flags().GetString(flagEngine)
+		if engine == "" {
+			if strings.Contains(targetBlockExplorer, "mintscan.io") {
+				engine = constants.EngineMintscan
+			} else if strings.Contains(targetBlockExplorer, "silknodes.io") {
+				engine = constants.EngineSilkNodes
+			} else {
+				fmt.Printf("Failed to detect block explorer engine, please specify it with --%s\n", flagEngine)
+				os.Exit(1)
+			}
+		}
+
+		redirectMessage, _ := cmd.Flags().GetString(flagRedirectMessage)
 		if redirectMessage == "" {
 			redirectMessage = fmt.Sprintf("Temporary unavailable, redirecting to %s", targetBlockExplorer)
 		}
@@ -98,7 +111,7 @@ var startCmd = &cobra.Command{
 			http.Redirect(w, r, targetBlockExplorer, http.StatusTemporaryRedirect)
 		})
 
-		fmt.Println("Starting HTTP server")
+		fmt.Printf("Starting HTTP server at :%d...\n", port)
 		http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	},
 }
@@ -106,7 +119,7 @@ var startCmd = &cobra.Command{
 func init() {
 	startCmd.Flags().StringP(flagRedirectMessage, "m", "", "Message to be displayed when redirecting")
 	startCmd.Flags().Uint16P(flagPort, "p", 8080, "Port to listen on")
-	startCmd.Flags().String(flagEngine, constants.EngineMintscan, "Block explorer engine to redirect to")
+	startCmd.Flags().String(flagEngine, "", fmt.Sprintf("Block explorer engine to redirect to, available options: %s, %s", constants.EngineMintscan, constants.EngineSilkNodes))
 	startCmd.Flags().Duration(flagRedirectTimeout, 3*time.Second, "Redirect timeout in seconds")
 	rootCmd.AddCommand(startCmd)
 }
